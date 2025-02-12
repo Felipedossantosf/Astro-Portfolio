48d01b62c597b1bf35215560417b5d22bdddee02
Felipe dos Santos <felipe28092001@gmail.com>
2025-02-12 18:30:58 -0300

    workflow added

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://Felipedossantosf.github.io",
+  base: "Astro-Portfolio",
   integrations: [tailwind()]
 });
