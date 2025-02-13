1be97d281244914fcb407ebbd91007004f561255
Felipe dos Santos <felipe28092001@gmail.com>
2025-02-12 21:11:22 -0300

    imgs fixed

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: "https://Felipedossantosf.github.io",
-  base: "Astro-Portfolio",
+  base: "/Astro-Portfolio/",
   integrations: [tailwind()]
 });
